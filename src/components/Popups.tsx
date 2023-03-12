6dea1e09405b054f54705a4d7161bc62f4dee71b
Jack <60209373+OpoJack@users.noreply.github.com>
2023-03-12 03:41:08 -0500

    Adjust social media links
    Share and Like buttons now always appear, regardless of other social medias

--- src/components/Popups.tsx
+++ src/components/Popups.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import type { Popup } from "~/types/types";
 import SocialMedia from "./SocialMedia";
 import Tag from "~/components/Tag";
+import { HeartIcon, ShareIcon } from "@heroicons/react/24/solid";
 
 export default function Popups({ popup }: { popup: Popup }) {
   const [isShown, setIsShown] = useState(false);
@@ -10,50 +11,47 @@ export default function Popups({ popup }: { popup: Popup }) {
     setIsShown(!isShown);
   };
   return (
-    <>
-      {/* {isShown && (
-        <PopupModal isShown={isShown} setIsShown={setIsShown} popup={popup} />
-      )} */}
-      <li
-        className="mx-2 flex flex-col divide-y divide-zinc-200 rounded-lg border bg-white shadow-sm transition-colors hover:bg-zinc-50 hover:shadow-md"
-        onClick={handleClick}
-      >
-        <div className="p-4">
-          <div className="flex flex-row space-x-2 object-cover">
-            <div className="flex space-y-3">
-              <Image
-                key={popup.links?.find((link) => link.name === "Instagram")?.id}
-                className="h-fit w-fit flex-shrink-0 rounded-full bg-gray-300"
-                src={popup.imageUrl ?? "/hotdog.jpg"}
-                alt=""
-                width={75}
-                height={75}
-              />
-            </div>
-            <div className="flex w-fit shrink flex-col">
-              <div className="flex h-5 flex-row space-x-2">
-                {popup.links?.map((link) => (
-                  <SocialMedia key={link.id} links={link} />
-                ))}
-              </div>
-              <div className="-mt-1 text-xl font-bold tracking-tight text-gray-900 antialiased">
-                {popup.name}
-              </div>
-              <div className="inline-block flex-shrink-0 text-xs font-normal text-gray-800">
-                {popup.description}
-              </div>
-              <div className="text-xs font-semibold text-gray-900">
-                Based in: {popup.basedIn}
-              </div>
-            </div>
+    <li
+      className="mx-2 flex flex-col divide-y divide-zinc-200 rounded-lg border bg-white shadow-sm transition-colors hover:bg-zinc-50 hover:shadow-md"
+      onClick={handleClick}
+    >
+      <div className="p-4">
+        <div className="flex flex-row space-x-2 object-cover">
+          <div className="flex space-y-3">
+            <Image
+              key={popup.links?.find((link) => link.name === "Instagram")?.id}
+              className="h-20 w-20 flex-shrink-0 rounded-full bg-gray-300"
+              src={popup.imageUrl ?? "/hotdog.jpg"}
+              alt=""
+              width={75}
+              height={75}
+            />
           </div>
-          <div className="flex flex-row space-x-1">
-            {popup.tags?.map((tag) => (
-              <Tag key={tag.id} name={tag?.name} />
-            ))}
+          <div className="flex w-fit shrink flex-col">
+            <div className="flex h-5 flex-row space-x-2">
+              {popup.links?.map((link) => (
+                <SocialMedia key={link.id} links={link} />
+              ))}
+              <ShareIcon fill="black" width={17} height={17} className="" />{" "}
+              <HeartIcon fill="red" width={17} height={17} className="" />
+            </div>
+            <div className="-mt-1 text-xl font-bold tracking-tight text-gray-900 antialiased">
+              {popup.name}
+            </div>
+            <div className="inline-block flex-shrink-0 text-xs font-normal text-gray-800">
+              {popup.description}
+            </div>
+            <div className="text-xs font-semibold text-gray-900">
+              Based in: {popup.basedIn}
+            </div>
           </div>
         </div>
-      </li>
-    </>
+        <div className="flex flex-row space-x-1">
+          {popup.tags?.map((tag) => (
+            <Tag key={tag.id} name={tag?.name} />
+          ))}
+        </div>
+      </div>
+    </li>
   );
 }
--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -1,26 +1,25 @@
-import { HeartIcon, ShareIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import type { Link } from "~/types/types";
 
 export default function SocialMedia({ links }: { links: Link }) {
+  return <InstagramLink links={links} />;
+}
+
+function InstagramLink({ links }: { links: Link }) {
   return (
-    <>
-      <a
-        target={"_blank"}
-        rel={"noreferrer"}
-        href={links.url ?? "/"}
+    <a
+      target={"_blank"}
+      rel={"noreferrer"}
+      href={links.url ?? "/"}
+      className="hover:cursor-pointer"
+    >
+      <Image
+        src={"/instagram.svg"}
+        width={17}
+        height={17}
         className="hover:cursor-pointer"
-      >
-        <Image
-          src={"/instagram.svg"}
-          width={17}
-          height={17}
-          className="hover:cursor-pointer"
-          alt="Instagram"
-        />
-      </a>
-      <ShareIcon fill="black" width={17} height={17} className="" />{" "}
-      <HeartIcon fill="red" width={17} height={17} className="" />
-    </>
+        alt="Instagram"
+      />
+    </a>
   );
 }
