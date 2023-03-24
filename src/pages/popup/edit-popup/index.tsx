769fc89ef92b3cfdbb8e55d3887d3f2130914ad5
Jack <60209373+OpoJack@users.noreply.github.com>
2023-03-23 20:08:01 -0500

    Add tags entry in Edit

--- src/pages/popup/edit-popup/index.tsx
+++ src/pages/popup/edit-popup/index.tsx
@@ -168,7 +168,7 @@ const EditPopup: NextPage = () => {
                                 name="name"
                                 id="popup-name"
                                 autoComplete="popup-name"
-                                className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                className="mt-2 block w-full rounded-md border-0 py-1.5  text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 defaultValue={popup?.name ? popup.name : ""}
                               />
                             )}
@@ -261,6 +261,31 @@ const EditPopup: NextPage = () => {
                                 <option>First come, first serve</option>
                                 <option>Preorder only</option>
                               </select>
+                            )}
+                          </div>
+                          {/* Based in */}
+                          <div className="col-span-6 sm:col-span-3">
+                            <label
+                              htmlFor="popup-name"
+                              className="block text-sm leading-6 text-gray-900"
+                            >
+                              {}
+                              Popup name
+                            </label>
+                            {/* Tag input */}
+                            {status === "loading" ? (
+                              <div className="flex justify-center">
+                                <Loading />
+                              </div>
+                            ) : (
+                              <input
+                                type="tags"
+                                name="name"
+                                id="popup-name"
+                                autoComplete="popup-name"
+                                className="mt-2 block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                defaultValue="Tags go here"
+                              />
                             )}
                           </div>
                         </div>
