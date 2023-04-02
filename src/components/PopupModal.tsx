55d970196fb979bb70805053c991005671c93c3f
Jack <60209373+OpoJack@users.noreply.github.com>
2023-04-02 11:06:03 -0500

    Color PopupModal

--- src/components/PopupModal.tsx
+++ src/components/PopupModal.tsx
@@ -29,7 +29,7 @@ export default function PopupModal({
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-gray-700 bg-opacity-25 backdrop-blur-sm transition-opacity" />
+          <div className="fixed inset-0 bg-secondary bg-opacity-20 backdrop-blur-sm transition-opacity" />
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 overflow-y-auto">
@@ -43,7 +43,7 @@ export default function PopupModal({
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative w-full max-w-xs transform overflow-hidden rounded-lg bg-white px-11 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8  sm:max-w-sm sm:p-6">
+              <Dialog.Panel className="relative w-full max-w-xs transform overflow-hidden rounded-lg bg-base-200 px-11 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8  sm:max-w-sm sm:p-6">
                 <div className="absolute right-4 top-2 flex space-x-2">
                   <SocialMedia links={popup.links} />
                 </div>
@@ -53,21 +53,21 @@ export default function PopupModal({
                       key={popup.id}
                       src={popup.imageUrl || "/hotdog.jpg"}
                       alt="Popup logo"
-                      height={125}
-                      width={125}
-                      className="h-fit w-fit flex-shrink-0 rounded-full bg-gray-300"
+                      height={75}
+                      width={75}
+                      className="h-1/2 w-1/2 flex-shrink-0 rounded-full"
                     />
                   </div>
                   <div className="mt-3 text-center sm:mt-5">
                     <Dialog.Title
                       as="h3"
-                      className="text-2xl font-medium leading-6 text-gray-900"
+                      className="text-2xl font-medium leading-6 text-primary"
                     >
                       {popup.name}
                     </Dialog.Title>
                     <div className="mt-1">
                       {/* TODO: Add a field for a US state to popup properties */}
-                      <p className="pb-5 text-sm text-gray-500">
+                      <p className="pb-5 text-sm text-primary-focus text-opacity-60">
                         {popup.basedIn}
                       </p>
 
@@ -80,7 +80,7 @@ export default function PopupModal({
                   <Link
                     href={`/popups/${popup.id}`}
                     type="button"
-                    className="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm"
+                    className="inline-flex w-full justify-center rounded-md border border-transparent bg-secondary px-4 py-2 text-base font-medium text-primary shadow-sm hover:bg-opacity-80 hover:text-primary-focus focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-1 sm:text-sm"
                     onClick={() => setOpen(false)}
                   >
                     View Popup
